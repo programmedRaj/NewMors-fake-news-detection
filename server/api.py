5d6dc628e19a695852d32701225fea62d4077261
rajsh <rajshah9172@gmail.com>
2020-11-25 15:16:06 +0530

    2 APIs ready

--- server/api.py
+++ server/api.py
@@ -23,37 +23,60 @@ import jsonpickle
 import numpy as np
 import cv2
 
+import requests
+from bs4 import BeautifulSoup
+
 CORS(app)
 
 
-@app.route("/recommendations", methods=["POST"])
-def recommendations():
-    conn = mysql.connect()
-    cur = conn.cursor(pymysql.cursors.DictCursor)
-    try:
-        cur.execute(
-            "Insert into admin(username,password) VALUES ('"
-            + str(request.json["username"])
-            + "','"
-            + generate_password_hash(str(request.json["password"]))
-            + "');"
-        )
-        conn.commit()
-        if cur:
-            resp = jsonify({"message": "success"})
-            resp.status_code = 200
-            return resp
-        resp = jsonify({"message": "Error."})
-        resp.status_code = 401
-        return resp
-    finally:
-        cur.close()
-        conn.close()
+@app.route("/categories", methods=["GET"])
+def get_cats():
+    URL = "https://www.aajtak.in/"
+    page = requests.get(URL)
+    topic = str(request.json["c_name"])
+    if topic == "मनोरंजन":
+        start = 37
+    if topic == "कोरोना":
+        start = 43
+    if topic == "कोरोना":
+        start = 42
+    if topic == "लाइफस्टाइल":
+        start = 42
+    if topic == "चुनाव":
+        start = 13
+    if topic == "भारत":
+        start = 42
+    if topic == "होम":
+        start = 42
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    j = soup.find_all("ul", class_="at-menu")
+    for i in j:
+        link = i.find("a", title=topic)["href"]
+    # print(link)
+    page1 = requests.get(link)
+    soup1 = BeautifulSoup(page1.content, "html.parser")
+    ola = {}
+    for i in soup1.find_all("li")[start:]:  # 37 manoranjan 42 corona
+        try:  # print(i)
+            ola[i.find("a")["title"]] = i.find("a")["href"]
+        except:
+            break
+    resp = jsonify(ola)
+    resp.status_code = 200
+    return resp
 
 
-@app.route("/edit", methods=["GET"])
-def ucounts():
-    resp = "h1"
+@app.route("/get-details", methods=["GET"])
+def details():
+    url = str(request.json["url"])
+    page = requests.get(url)
+    soups = BeautifulSoup(page.content, "html.parser")
+    j = " "
+    for i in soups.find_all("p"):
+        j = j + i.getText()
+    details = j.replace("Feedback", "")
+    resp = details
     return resp
 
 
@@ -68,6 +91,31 @@ def not_found(error=None):
     resp.status_code = 404
 
     return resp
+
+
+# @app.route("/recommendations", methods=["POST"])
+# def recommendations():
+#     conn = mysql.connect()
+#     cur = conn.cursor(pymysql.cursors.DictCursor)
+#     try:
+#         cur.execute(
+#             "Insert into admin(username,password) VALUES ('"
+#             + str(request.json["username"])
+#             + "','"
+#             + generate_password_hash(str(request.json["password"]))
+#             + "');"
+#         )
+#         conn.commit()
+#         if cur:
+#             resp = jsonify({"message": "success"})
+#             resp.status_code = 200
+#             return resp
+#         resp = jsonify({"message": "Error."})
+#         resp.status_code = 401
+#         return resp
+#     finally:
+#         cur.close()
+#         conn.close()
 
 
 if __name__ == "__main__":
--- server/hey.py
+++ server/hey.py
@@ -1,39 +1,42 @@
-#मनोरंजन,कोरोना,कोरोना,लाइफस्टाइल,चुनाव,भारत,होम
-topic=
+# मनोरंजन,कोरोना,कोरोना,लाइफस्टाइल,चुनाव,भारत,होम
+
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.aajtak.in/'
+URL = "https://www.aajtak.in/"
 page = requests.get(URL)
 
-if topic=='मनोरंजन':
-    start=37
-if topic=='कोरोना':
-    start=43
-if topic=='कोरोना':
-    start=42 
-if topic=='लाइफस्टाइल':
-    start=42    
-if topic=='चुनाव':
-    start=13   
-if topic=='भारत':
-    start=42   
-if topic=='होम':
-    start=42    
-    
-    
-soup = BeautifulSoup(page.content, 'html.parser')
-j=soup.find_all('ul', class_='at-menu')
+
+topic = "मनोरंजन"
+
+if topic == "मनोरंजन":
+    start = 37
+if topic == "कोरोना":
+    start = 43
+if topic == "कोरोना":
+    start = 42
+if topic == "लाइफस्टाइल":
+    start = 42
+if topic == "चुनाव":
+    start = 13
+if topic == "भारत":
+    start = 42
+if topic == "होम":
+    start = 42
+
+
+soup = BeautifulSoup(page.content, "html.parser")
+j = soup.find_all("ul", class_="at-menu")
 for i in j:
-    link=i.find('a',title=topic)['href']
-#print(link)    
+    link = i.find("a", title=topic)["href"]
+# print(link)
 page1 = requests.get(link)
-soup1= BeautifulSoup(page1.content, 'html.parser')
-ola={}
-for i in soup1.find_all('li')[start:]:    #37 manoranjan 42 corona
-    try:#print(i)
-        ola[i.find('a')['title']]=i.find('a')['href']    
+soup1 = BeautifulSoup(page1.content, "html.parser")
+ola = {}
+for i in soup1.find_all("li")[start:]:  # 37 manoranjan 42 corona
+    try:  # print(i)
+        ola[i.find("a")["title"]] = i.find("a")["href"]
     except:
         break
-        
-#ola is main
+
+# ola is main
